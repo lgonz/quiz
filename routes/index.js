888b7d3ec7f25b158ec370a0754f8d5f91d81035
Lorena <lore22lgc@gmail.com>
2015-07-13 00:23:28 +0200

    Página 'Creditos'

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET author page. */
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Créditos' });
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
