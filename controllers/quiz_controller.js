3d6a4914378fca59cb34bb5a4c0f9f1d45f0438c
Lorena <lore22lgc@gmail.com>
2015-08-01 21:44:36 +0200

    v0.13 - Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -69,14 +69,30 @@ exports.create = function(req, res) {
 	);
 };
 
-/*
-// POST /quizes/create
-exports.create = function(req, res) {
-	var quiz = models.Quiz.build( req.body.quiz );
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+	var quiz = req.quiz; // autoload de instancia de quiz
 
-	//guarda en DB los cammpos pregunta y respuesa de quiz
-	quiz.save({fields: ['pregunta', 'respuesta']}).then(function() {
-		res.redirect('/quizes');
-	})	// Redirección HTTP (URL relativo) lista de preguntas
+	res.render('quizes/edit', {quiz: quiz, errors: []});
 };
-*/
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz
+	.validate()
+	.then(
+		function (err) {
+			if (err) {
+				res.render('quizes/edit', {quiz: req.quiz,
+										   errors: err.errors});
+			} else {
+				req.quiz
+				.save( {fields: ['pregunta', 'respuesta']})
+				.then( function(){ res.redirect('/quizes');});
+			}	// Redirección HTTP a lista de preguntas (URL relativo)
+		}
+	);
+};
