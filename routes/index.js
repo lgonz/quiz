d429ce7f62ece2751491ef012e0d1624771d3616
Lorena <lore22lgc@gmail.com>
2015-08-07 20:07:38 +0200

    v0.18 - Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,8 @@ router.get('/author', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);	// autoload :quizId
+router.param('commentId', commentController.load);	// autoload :commentId
+
 
 // Definición de rutas de sesión
 router.get('/login', 		sessionController.new);	// formulario login
@@ -33,7 +35,10 @@ router.get('/quizes/:quizId(\\d+)/edit', 	sessionController.loginRequired, quizC
 router.put('/quizes/:quizId(\\d+)', 		sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)', 		sessionController.loginRequired, quizController.destroy);
 
+// Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', 	commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', 		commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 		
+													commentController.publish);
 
 module.exports = router;
